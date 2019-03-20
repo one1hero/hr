76603317305c5dc8bea2be87449511d876e0fef0
liubin <liubin.b@qq.com>
2019-03-20 15:39:47 +0800

    修改HomeController

--- app/src/main/java/com/newer/app/HomeController.java
+++ app/src/main/java/com/newer/app/HomeController.java
@@ -3,6 +3,12 @@ package com.newer.app;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+/**
+ * 
+ * @author LiuBin
+ *
+ */
 @RestController
 public class HomeController {
 
